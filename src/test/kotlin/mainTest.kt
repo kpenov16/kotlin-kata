ca2d67eff0154cde35cc0aea2cf7ec56b8379d0f
kpenov16 <s133967@student.dtu.dk>
2020-11-13 00:36:00 +0100

    add string '1,2'

--- src/test/kotlin/mainTest.kt
+++ src/test/kotlin/mainTest.kt
@@ -8,7 +8,23 @@ class mainTest {
     fun givenEmptyString_returnZero(){
         assertEquals(0, add(""));
     }
+    @Test
+    fun givenOneNumber_returnNumber(){
+        assertEquals(1, add("1"));
+    }
+    @Test
+    fun givenTwoNumbers_returnSum(){
+        assertEquals(3, add("1,2"));
+    }
     fun add(str:String): Int{
-        return 0;
+        if(str.isNullOrEmpty())
+            return 0
+        else if(str.length == 1 && str.toIntOrNull() != null)
+            return str.toInt();
+        else if(str.contains(",")){
+            val nums : List<String> = str . split (",");
+            return nums.map { s -> s.toInt() }.sum();
+        }else
+            return 1;
     }
 }
